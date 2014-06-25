20cfd75c253d49075eb049f422a3c63fd159686c
JP Simard <jp@jpsim.com>
2014-06-25 08:06:45 -0700

    WIP JSON importer

---
+++ tools/realm-import/README.md
@@ -0,0 +1,14 @@
+# Realm Import
+
+`realm-import` is a command-line tool written in Objective-C that allows realm files to be created from input JSON files.
+
+## Usage
+
+```
+$ realm-import "my file.json"
+output.realm was successfully created with data from "my file.json".
+```
+
+## License
+
+`realm-import` is MIT-licensed. It uses KZPropertyMapper by Krzysztof Zabłocki, which is also MIT-licensed.
---
+++ tools/realm-import/realm-import/vendor/KZPropertyMapper/KZPropertyMapperCommon.m
@@ -0,0 +1,15 @@
+//
+//  Created by merowing on 08/10/2013.
+//
+//
+//
+#import "KZPropertyMapperCommon.h"
+
+NSError *pixle_NSErrorMake(NSString *message, NSUInteger code, NSDictionary *aUserInfo, SEL selector) {
+  NSMutableDictionary *userInfo = [NSMutableDictionary dictionaryWithDictionary:aUserInfo];
+  userInfo[NSLocalizedDescriptionKey] = message;
+  NSError *error = [NSError errorWithDomain:@"com.pixle.KZPropertyMapper" code:code userInfo:userInfo];
+
+  NSLog(@"KZPropertyMapper Error: %@", error.localizedDescription);
+  return error;
+}
